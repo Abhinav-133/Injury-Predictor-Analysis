c37f94e1c3a9cfd72c14421ab3b7f0315d92a482
Abhinav Mittal <114458742+Abhinav-133@users.noreply.github.com>
2024-11-25 19:27:03 +0000

    ok

--- app.py
+++ app.py
@@ -158,7 +158,12 @@ def add_predictions(input_data):
         st.subheader("Injury Prediction Results")
         st.write("Based on your input parameters, here are the results:")
 
-        if prediction_proba[0][0] >= 0.5:
+        if prediction_proba[0][0] < 0.5:
+            st.error("**High risk: Injury is likely**")
+        else:
+            st.success("**Low risk: Injury is unlikely**")
+
+        if prediction[0] == 0.0:
             st.error("**High risk: Injury is likely**")
         else:
             st.success("**Low risk: Injury is unlikely**")
